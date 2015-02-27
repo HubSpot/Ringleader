9947f9d07b44fe65edffdb3bb17d3fdbe0b0ab17
Jonathan Haber <jhaber@hubspot.com>
2015-02-27 11:30:42 -0500

    Add blocking watcher

--- src/main/java/com/hubspot/ringleader/watcher/WatcherFactory.java
+++ src/main/java/com/hubspot/ringleader/watcher/WatcherFactory.java
@@ -3,6 +3,8 @@ package com.hubspot.ringleader.watcher;
 import com.google.common.base.Supplier;
 import org.apache.curator.framework.CuratorFramework;
 
+import java.util.concurrent.CountDownLatch;
+
 public class WatcherFactory {
   private final Supplier<CuratorFramework> curatorSupplier;
 
@@ -12,5 +14,32 @@ public class WatcherFactory {
 
   public PersistentWatcher dataWatcher(String path) {
     return new PersistentWatcher(curatorSupplier, path);
+  }
+
+  public PersistentWatcher blockingDataWatcher(String path) {
+    final CountDownLatch started = new CountDownLatch(1);
+
+    PersistentWatcher watcher = new PersistentWatcher(curatorSupplier, path) {
+
+      @Override
+      public void start() {
+        super.start();
+        try {
+          started.await();
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+        }
+      }
+    };
+
+    watcher.getEventListenable().addListener(new EventListener() {
+
+      //@Override Java 5 compatibility
+      public void newEvent(Event event) {
+        started.countDown();
+      }
+    });
+
+    return watcher;
   }
 }
